475c929eccbcd324a5904469c0d06645f14ebcb5
Dylan Neve <dylanneve1@gmail.com>
2021-11-30 14:35:55 +0000

    Inherit vendor BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,3 +27,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 4561305600
 
 # Inherit common board flags
 include device/samsung/universal9810-common/BoardConfigCommon.mk
+
+# Inherit common board flags
+include device/samsung/starlte/BoardConfigVendor.mk
